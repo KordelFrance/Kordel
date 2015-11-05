de82e8707bdec2fccc534a4da1a120813fd48950
Kordel France <kordel@looplist.com>
2015-11-05 15:19:51 -0700

    Initial Project Opening--Renamed ViewController to SearchViewController

---
+++ LoopSearch/SearchViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoopSearch
+//
+//  Created by Kade France on 11/5/15.
+//  Copyright © 2015 Kade France. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
