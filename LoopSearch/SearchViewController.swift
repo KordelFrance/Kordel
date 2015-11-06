ecd581ce4aa64b8b384566d23e426e0d373fca88
Kordel France <kordel@looplist.com>
2015-11-06 01:38:43 -0700

    various search functions and delegates

--- LoopSearch/SearchViewController.swift
+++ LoopSearch/SearchViewController.swift
@@ -12,11 +12,13 @@ class SearchViewController: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     
+    var searchResults = [SearchResult]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
+        tableView.contentInset = UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0)
         //searchBar.contentInset = UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0)
     }
 
@@ -27,19 +29,48 @@ class SearchViewController: UIViewController {
     
 }
 
+//instantiat a new [String] array and put into searchResults instance variable. Done each time a user performs a search
 extension SearchViewController: UISearchBarDelegate{
     func searchBarSearchButtonClicked(searchBar: UISearchBar) {
-        print("The search text is: '\(searchBar.text!)'")
+        //make the keyboard disappear after user clicks "search"
+        searchBar.resignFirstResponder()
+        
+        searchResults = [SearchResult]()
+        
+        for i in 0...2 {
+            let searchResult = SearchResult()
+            searchResult.name = String(format: "Fake Result %d for ", i)
+            
+            //comment when integrating indix
+            searchResult.artistName = searchBar.text!
+            searchResults.append(searchResult)
+        }
+        
+        tableView.reloadData()
+    }
+    
+    //connect top of search bar to top data bar
+    func positionForBar(bar: UIBarPositioning) -> UIBarPosition {
+        return .TopAttached
     }
 }
 
 extension SearchViewController: UITableViewDataSource {
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return searchResults.count
     }
     
     func tableView (tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cellIdentifier = "SearchResultCell"
+        var cell: UITableViewCell! = tableView.dequeueReusableCellWithIdentifier(cellIdentifier)
+        if cell == nil {
+            cell = UITableViewCell(style: .Default, reuseIdentifier: cellIdentifier)
+        }
+        
+        let searchResult = searchResults[indexPath.row]
+        cell.textLabel!.text = searchResult.name
+        cell.detailTextLabel!.text = searchResult.artistName
+        return cell
     }
 }
 
---
+++ SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  LoopSearch
+//
+//  Created by Kade France on 11/6/15.
+//  Copyright © 2015 Kade France. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
